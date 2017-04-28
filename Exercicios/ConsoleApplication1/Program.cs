b8e8003701f11020b7ae8dec3d7576708b07ceb6
GuilhermeDaSilva <kojofx@gmail.com>
2017-04-27 23:21:50 -0300

    Codigo com revisão de sub rotinas.

--- Exercicios/ConsoleApplication1/Program.cs
+++ Exercicios/ConsoleApplication1/Program.cs
@@ -11,12 +11,12 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             int N;
-            string modelo,tipo,tipod;
-            double km,mot;
-            
-            Console.WriteLine("Informe a quantidade de carros no galpão");
+            string modelo,valores;
+            double km, mot;
+
+            Console.WriteLine("Informe o número de carros no galpão");
             N = Convert.ToInt32(Console.ReadLine());
-                        
+                                    
             for (int i = 0; i < N; i++)
             {
                 Console.WriteLine("Informe o modelo");
@@ -28,35 +28,42 @@ namespace ConsoleApplication1
                 Console.WriteLine("Informe a potência do motor");
                 mot = Double.Parse(Console.ReadLine());
 
-                if (km <= 5000)
-                {
-                    tipo = "novo";
-                }
-                else if (km > 5000 && km <= 30000)
-                {
-                    tipo = "seminovo";
-                }
-                else
-                {
-                    tipo = "velho";
-                }              
+                valores= Classificar(modelo,km,mot);
                 
-                if (mot > 200)
-                {
-                    tipod = "potente";
-                }
-                else if (mot >= 120 && mot <= 200)
-                {
-                    tipod = "forte";
-                }
-                else
-                {
-                    tipod = "popular";
-                }               
-
-                Console.WriteLine("{0} - {1} {2}", modelo, tipo, tipod);
+                Console.WriteLine("{0}",valores);
             }
 
+        }
+        static string Classificar(string mod, double klm, double motorpot)
+        {
+            string tipo, tipod, tudo;
+            if (klm <= 5000)
+            {
+                tipo = "novo";
+            }
+            else if (klm > 5000 && klm <= 30000)
+            {
+                tipo = "seminovo";
+            }
+            else
+            {
+                tipo = "velho";
+            }
+
+            if (motorpot > 200)
+            {
+                tipod = "potente";
+            }
+            else if (motorpot >= 120 && motorpot <= 200)
+            {
+                tipod = "forte";
+            }
+            else
+            {
+                tipod = "popular";
+            }
+            tudo = mod + " - " + tipo + " " + tipod;
+            return tudo;            
         }
     }
 }
