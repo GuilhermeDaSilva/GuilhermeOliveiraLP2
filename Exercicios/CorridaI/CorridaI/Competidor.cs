b67eb4ba01cb4b138b0b2df9642d9c195e4b10db
Guilherme da Silva de Oliveira <kojfox@gmail.com>
2017-06-13 20:03:06 -0300

    Mandei a corrida errada, a certa está AQUI!

---
+++ Exercicios/CorridaI/CorridaI/Competidor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorridaI
+{
+    class Comp
+    {
+        public double prim;
+        public double seg;
+        public double terc;
+        public string nome;
+
+        public double contaf()
+        {
+            double[] tempos = new double[3] { prim, seg, terc };
+            Array.Sort(tempos);
+            double tudo = tempos[0] + tempos[1];
+            return tudo;
+        }
+    }
+}
---
+++ Exercicios/CorridaI/CorridaI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorridaI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = int.Parse(Console.ReadLine());
+            Comp[] comps = new Comp[no];
+            int i;
+            double Tempomenos = double.MaxValue;
+            string vencedor;
+            for (i = 0; i < no; i++)
+            {
+                comps[i] = new Comp();
+                comps[i].nome = Console.ReadLine();
+            }
+            for (i = 0; i < no; i++)
+            {
+                Console.WriteLine("Informe o tempo da primeira etapa do competidor {0}", comps[i].nome);
+                comps[i].prim = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o tempo da segunda etapa do competidor {0}", comps[i].nome);
+                comps[i].seg = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o tempo da terceira etapa do competidor {0}", comps[i].nome);
+                comps[i].terc = Double.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < no; i++)
+            {
+                if (comps[i].contaf() < Tempomenos)
+                {
+                    Tempomenos = comps[i].contaf();
+                    vencedor = comps[i].nome;
+                }
+            }
+            Console.WriteLine("{0}", vencedor);   
+        }
+    }
+}
