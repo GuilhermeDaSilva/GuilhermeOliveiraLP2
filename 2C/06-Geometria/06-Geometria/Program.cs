61810a96eb0a632346a016e1e984d65704d8972a
Guilherme da Silva de Oliveira <kojofx@gmail.com>
2017-08-30 17:56:18 -0300

    Geometria e ConversorMedidas

---
+++ 2C/06-Geometria/06-Geometria/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Geometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+
+            Triangulo tr = new Triangulo();
+            Console.WriteLine(tr.area(b, h));
+        }
+        
+    }
+}
---
+++ 2C/06-Geometria/06-Geometria/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Geometria
+{
+    class Triangulo
+    {
+        public double ar;
+        public double area(int b, int h)
+        {
+            ar = (b * h) / 2;
+            return (ar);
+        }
+    }
+}
